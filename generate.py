c0501308b14be72ee53bf41b677c88948852aa4c
InsideousBot <smaran.manchala@gmail.com>
2024-11-05 20:59:52 -0600

    Assignment E - Joints

--- generate.py
+++ generate.py
@@ -1,17 +1,21 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
 x = 1
 y = 1
 z = 1
-for i in range(5):
-    for j in range(5):
-        x = 1
-        y = 1
-        z = 1
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box1", pos=[i, j, k + .5], size=[x, y, z])
-            x = .9*x
-            y = .9*y
-            z = .9*z
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box1", pos=[2, 2, .5], size=[x, y, z])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Link0", pos=[0, 0, .5], size=[x, y, z])
+    pyrosim.Send_Cube(name="Link1", pos=[.5, 0, .5], size=[x, y, z])
+    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[.5, 0, 1])
+    pyrosim.Send_Cube(name="Link2", pos=[.5, 0, -.5], size=[x, y, z])
+    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[1, 0, 0])
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
